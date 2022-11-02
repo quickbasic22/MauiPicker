fd7df307bbe12f41d12ffcb0853a0a22e7ce28a5
David Morrow <quickbasic44@outlook.com>
2022-11-02 16:47:23 -0400

    Added IndicatorView with Squares at 20 size

--- MauiPicker/PictureCarousel.xaml.cs
+++ MauiPicker/PictureCarousel.xaml.cs
@@ -15,7 +15,13 @@ public partial class PictureCarousel : ContentPage
 			new PictureModel() { PictureObject = PictureObject.Person, Date = DateTime.Now.AddDays(-1250), Description = "First Picture", PictureUrl = ImageSource.FromFile("tshirt197350years.png") },
 			new PictureModel() { PictureObject = PictureObject.Object, Date = DateTime.Now.AddDays(-126), Description = "Second Picture", PictureUrl = ImageSource.FromFile("ssdpci.jpg") },
 			new PictureModel() { PictureObject = PictureObject.Location, Date = DateTime.Now.AddDays(1250), Description = "Third Picture", PictureUrl = ImageSource.FromFile("ohiotunnel.jpg") },
-		};
+            new PictureModel() { PictureObject = PictureObject.Person, Date = DateTime.Now.AddDays(-1250), Description = "First Picture", PictureUrl = ImageSource.FromFile("tshirt197350years.png") },
+            new PictureModel() { PictureObject = PictureObject.Object, Date = DateTime.Now.AddDays(-126), Description = "Second Picture", PictureUrl = ImageSource.FromFile("ssdpci.jpg") },
+            new PictureModel() { PictureObject = PictureObject.Location, Date = DateTime.Now.AddDays(1250), Description = "Third Picture", PictureUrl = ImageSource.FromFile("ohiotunnel.jpg") },
+            new PictureModel() { PictureObject = PictureObject.Person, Date = DateTime.Now.AddDays(-1250), Description = "First Picture", PictureUrl = ImageSource.FromFile("tshirt197350years.png") },
+            new PictureModel() { PictureObject = PictureObject.Object, Date = DateTime.Now.AddDays(-126), Description = "Second Picture", PictureUrl = ImageSource.FromFile("ssdpci.jpg") },
+            new PictureModel() { PictureObject = PictureObject.Location, Date = DateTime.Now.AddDays(1250), Description = "Third Picture", PictureUrl = ImageSource.FromFile("ohiotunnel.jpg") },
+        };
 		BindingContext = this;
 	}
 
@@ -31,6 +37,11 @@ public partial class PictureCarousel : ContentPage
 			myFrame.Content = img;
 			return myFrame;
 		});
+		
+    }
 
+	private void ToolbarItem_Clicked(object sender, EventArgs e)
+	{
+        MyCarousel.ItemTemplate = MyDataTemplate;
     }
 }
